eb5657c056553ffeb7a0f20074ba407f0eddd59f
PeiyiChi <peiyi.chi@taitradio.com>
2018-10-03 10:28:39 +1300

    Adding debug log
    Signed-off-by: PeiyiChi <peiyi.chi@taitradio.com>

--- src/emqx_auth_http_cli.erl
+++ src/emqx_auth_http_cli.erl
@@ -27,6 +27,7 @@ request(get, Url, Params) ->
     {_, Username} = lists:keyfind("username", 1, Params),
     Realm = lists:nth(1, string:split(Username, "_")),
     Req = {string:replace(Url, "REALM", Realm), [{"Authorization", "Bearer " ++ Token}, {"albi-client-type", "albi_internal"}]},
+    io:format("Sending HTTP Get Request to id-rest:~p~n", [Req]),
     reply(request_(get, Req, [{autoredirect, true}], [], 0));
 
 request(post, Url, Params) ->
