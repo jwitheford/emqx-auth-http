782443d407633c257d3e0f0889d5cf8d68ace060
PeiyiChi <peiyi.chi@taitradio.com>
2018-10-03 14:33:47 +1300

    Moved the code change from emq30 to emq24 branch

--- src/emq_auth_http_cli.erl
+++ src/emq_auth_http_cli.erl
@@ -25,7 +25,11 @@
 %%--------------------------------------------------------------------
 
 request(get, Url, Params) ->
-    Req = {Url ++ "?" ++ mochiweb_util:urlencode(Params), []},
+    {_, Token} = lists:keyfind("password", 1, Params),
+    {_, Username} = lists:keyfind("username", 1, Params),
+    Realm = lists:nth(1, string:split(Username, "_")),
+    Req = {string:replace(Url, "REALM", Realm), [{"Authorization", "Bearer " ++ Token}, {"albi-client-type", "albi_internal"}]},
+    io:format("Sending HTTP Get Request to id-rest:~p~n", [Req]),
     reply(request_(get, Req, [{autoredirect, true}], [], 0));
 
 request(post, Url, Params) ->
