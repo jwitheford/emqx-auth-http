ce94939201bb855f0db9a2938f6f312ad6516d3f
PeiyiChi <peiyi.chi@taitradio.com>
2018-10-03 17:28:58 +1300

    Use string:concat instead of ++  to concatenate string

--- src/emqx_auth_http_cli.erl
+++ src/emqx_auth_http_cli.erl
@@ -26,7 +26,7 @@ request(get, Url, Params) ->
     {_, Token} = lists:keyfind("password", 1, Params),
     {_, Username} = lists:keyfind("username", 1, Params),
     Realm = lists:nth(1, string:split(Username, "_")),
-    Req = {string:replace(Url, "REALM", Realm), [{"Authorization", "Bearer " ++ Token}, {"albi-client-type", "albi_internal"}]},
+    Req = {string:replace(Url, "REALM", Realm), [{"Authorization", string:concat("Bearer ", Token)}, {"albi-client-type", "albi_internal"}]},
     io:format("Sending HTTP Get Request to id-rest:~p~n", [Req]),
     reply(request_(get, Req, [{autoredirect, true}], [], 0));
 
